318593775ad559d5229fb6983f8b159e5798fdfa
Simeon <simeonogunyinka@gmail.com>
2025-05-28 17:24:15 +0100

    Trying to fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build: {
+    target: 'esnext', // this allows top-level await
+  },
   plugins: [
     tailwindcss(),
   ],
